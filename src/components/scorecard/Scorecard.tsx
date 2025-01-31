1e4d1057d9b818162ce2c44f4dc330b22fc636b8
Ben Stapleton <webdev.ben@gmail.com>
2025-01-31 16:10:37 +0000

    Bugfix to stop rendering answer selection details for statements

--- src/components/scorecard/Scorecard.tsx
+++ src/components/scorecard/Scorecard.tsx
@@ -1,7 +1,7 @@
 import {IScorecard} from "./IScorecard.ts";
 import {IFlashcardAnswer} from "./IFlashcardAnswer.ts";
 import styles from './Scorecard.module.css';
-import {getQuestionTypeName} from "../../types/questionType.ts";
+import {getQuestionTypeName, QuestionType} from "../../types/questionType.ts";
 import {Check} from "../glyphs/check.tsx";
 import {Colour} from "../../types/colour.ts";
 import {Times} from "../glyphs/times.tsx";
@@ -32,39 +32,41 @@ const Scorecard = ({ handleSubmission, data }: ScorecardProps) => {
                 <p>New difficulty: {data.new_difficulty}</p>
                 <p>Earliest you'll see this question again: {data.next_eligible_at ? new Date(Date.parse(data.next_eligible_at.toString())).toLocaleString() : null}</p>
             </div>
-            <div className={styles.bodyFull}>
-                (<Check symbolOnly={true} small={true} colour={Colour.GREEN} /> correct selection) (<Times symbolOnly={true} small={true} colour={Colour.RED} /> incorrect selection) (<Minus symbolOnly={true} small={true} colour={Colour.YELLOW} /> correct but not selected)
-                <ul>
-                    {data.flashcard_answers.map((flashcardAnswer: IFlashcardAnswer) => (
-                        <li key={flashcardAnswer.id} className={styles.answer}>
-                            {flashcardAnswer.was_selected && flashcardAnswer.is_correct ? (
-                                <Check symbolOnly={false} colour={Colour.GREEN} />
-                            ) : null}
-
-                            {flashcardAnswer.was_selected && !flashcardAnswer.is_correct ? (
-                                <Times symbolOnly={false} colour={Colour.RED} />
-                            ) : null}
-
-                            {!flashcardAnswer.was_selected && flashcardAnswer.is_correct ? (
-                                <Minus symbolOnly={false} colour={Colour.YELLOW} />
-                            ) : null}
-                            <div className={styles.text}>
-                                <span className={'font-bold'}>{flashcardAnswer.text}</span>
-                                {flashcardAnswer.explanation ? (
-                                    <span><br />{flashcardAnswer.explanation}</span>
+            {data.type !== QuestionType.Statement ? (
+                <div className={styles.bodyFull}>
+                    (<Check symbolOnly={true} small={true} colour={Colour.GREEN} /> correct selection) (<Times symbolOnly={true} small={true} colour={Colour.RED} /> incorrect selection) (<Minus symbolOnly={true} small={true} colour={Colour.YELLOW} /> correct but not selected)
+                    <ul>
+                        {data.flashcard_answers.map((flashcardAnswer: IFlashcardAnswer) => (
+                            <li key={flashcardAnswer.id} className={styles.answer}>
+                                {flashcardAnswer.was_selected && flashcardAnswer.is_correct ? (
+                                    <Check symbolOnly={false} colour={Colour.GREEN} />
                                 ) : null}
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-            {data.previous_attempt.attemptedAt ?
+
+                                {flashcardAnswer.was_selected && !flashcardAnswer.is_correct ? (
+                                    <Times symbolOnly={false} colour={Colour.RED} />
+                                ) : null}
+
+                                {!flashcardAnswer.was_selected && flashcardAnswer.is_correct ? (
+                                    <Minus symbolOnly={false} colour={Colour.YELLOW} />
+                                ) : null}
+                                <div className={styles.text}>
+                                    <span className={'font-bold'}>{flashcardAnswer.text}</span>
+                                    {flashcardAnswer.explanation ? (
+                                        <span><br />{flashcardAnswer.explanation}</span>
+                                    ) : null}
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            ) : null}
+            {data.previous_attempt.attemptedAt ? (
                 <div className={styles.body}>
                     <h3 className={styles.previous}>Previous attempt</h3>
                     <p>At: {data.previous_attempt.attemptedAt.toString()}</p>
                     <p>Correctness: {data.previous_attempt.correctness}</p>
                 </div>
-            : null}
+            ) : null}
             <footer>
                 <button onClick={handleSubmit}>Next question</button>
             </footer>
