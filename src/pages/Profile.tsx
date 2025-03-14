484c282dc7e4c6f2ccc015cb5c6db528bb55ead2
Ben Stapleton <webdev.ben@gmail.com>
2025-03-14 12:01:10 +0000

    Adds placeholder pages

---
+++ src/pages/Profile.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <main>
+            <h1>Import</h1>
+        </main>
+    );
+}
+// TODO add a link to the import page as well as everything else
+export default Profile;
