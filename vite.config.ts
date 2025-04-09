a269ca12839deeff5fd0bc799a19cdfb5c7b2a61
Ben Stapleton <webdev.ben@gmail.com>
2025-04-09 07:33:00 +0100

    Revert "Let's try that again"
    This reverts commit 4718c6cb63ce9c5ff78e1df7a2cb901757804112.

--- vite.config.ts
+++ vite.config.ts
@@ -12,12 +12,7 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      external: ['react', 'react-router', 'react-router-dom', 'react-redux'],
-      output: {
-        globals: {
-          react: 'React',
-        }
-      }
+      external: ['react-router', 'react-router-dom', 'react-i18next'],
     }
   }
 })
