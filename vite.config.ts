cce2167c2e25ee7660de68fd08cfe1c18960e560
Ben Stapleton <webdev.ben@gmail.com>
2025-04-09 07:27:23 +0100

    And again

--- vite.config.ts
+++ vite.config.ts
@@ -12,12 +12,7 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      external: ['react', 'react-router', 'react-router-dom', 'react-redux'],
-      output: {
-        globals: {
-          react: 'React',
-        }
-      }
+      external: ['react-router']
     }
   }
 })
