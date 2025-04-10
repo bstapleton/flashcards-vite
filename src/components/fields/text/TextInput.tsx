6ff0f1bfc2b99553c75f6813e7333c29bd6963fa
Ben Stapleton <webdev.ben@gmail.com>
2025-04-10 11:37:37 +0100

    Update text field component to allow for different types
    As well as handle state changes

--- src/components/fields/text/TextInput.tsx
+++ src/components/fields/text/TextInput.tsx
@@ -1,25 +1,39 @@
 import styles from './TextInput.module.css'
+import {TextFieldType} from "./TextFieldType.ts";
+import React from "react";
 
 type TextInputProps = {
     label: string;
+    value?: string;
+    type: TextFieldType
     id: string;
     validationRule?: string;
     hint?: string;
+    onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void
 }
 
 export default function TextInput(props: TextInputProps) {
+    const [value, setValue] = React.useState(props.value);
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(event.target.value);
+        if (props.onChange) {
+            props.onChange(event);
+        }
+    };
+
     return (
         <div className={styles.field}>
-            <label
-                htmlFor={props.id}
-                className={styles.label}>
+            <label htmlFor={props.id} className={styles.label}>
                 {props.label}
             </label>
             {props.hint ? (
                 <>
-                    <input type="text"
+                    <input type={props.type}
                            id={props.id}
                            className={styles.input}
+                           value={value}
+                           onChange={handleChange}
                            aria-describedby={props.id + '-hint'}
                     />
                     <p id={props.id + '-hint'} className={styles.hint}>
@@ -27,9 +41,11 @@ export default function TextInput(props: TextInputProps) {
                     </p>
                 </>
             ) : (
-                <input type="text"
-                        id={props.id}
-                    className={styles.input}
+                <input type={props.type}
+                       id={props.id}
+                       value={value}
+                       onChange={handleChange}
+                       className={styles.input}
                 />
             )}
         </div>
